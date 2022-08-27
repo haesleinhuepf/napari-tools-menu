2cacf4f5c34a81ffa726cca538046db7c3abe131
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-08-27 09:40:50 +0200

    catch error and warn
    Potential fix for:
    https://github.com/haesleinhuepf/napari-simpleitk-image-processing/issues/15

--- napari_tools_menu/__init__.py
+++ napari_tools_menu/__init__.py
@@ -6,7 +6,11 @@ Replace code below according to your needs.
 """
 import warnings
 import napari
-import napari._qt
+try:
+    import napari._qt
+except:
+    warnings.warn("importing napari._qt failed")
+    pass
 import numpy as np
 from qtpy.QtWidgets import QMenu
 from qtpy.QtCore import QTimer
